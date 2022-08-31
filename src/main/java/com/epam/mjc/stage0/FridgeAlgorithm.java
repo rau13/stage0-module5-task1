f30949a1babadba58c9463ba02181c7683edf1cf
rau1310 <rmusakhan@college-narxoz.kz>
2022-08-31 19:59:24 +0600

    changed FridgeAlgorithm

--- src/main/java/com/epam/mjc/stage0/FridgeAlgorithm.java
+++ src/main/java/com/epam/mjc/stage0/FridgeAlgorithm.java
@@ -4,8 +4,8 @@ import com.epam.mjc.stage0.utils.Fridge;
 
 import java.util.LinkedList;
 
-public class FridgeAlgorithm implements Fridge{
-    private final LinkedList<Actions> actionsQueue = new LinkedList<>();
+public class FridgeAlgorithm{
+
     /**
      * This is a first task in this module. You need to get milk from a fridge =)
      * <p>
@@ -13,29 +13,8 @@ public class FridgeAlgorithm implements Fridge{
      * P.S All methods of interaction with the fridge can be found inside the {@see Fridge} class.
      */
     public void fridgeAlgorithm(Fridge fridge) {
-        open();
-        getMilk();
-        close();
-    }
-
-
-    @Override
-    public void close() {
-        actionsQueue.add(Actions.CLOSE);
-    }
-
-    @Override
-    public void open() {
-        actionsQueue.add(Actions.OPEN);
-    }
-
-    @Override
-    public void getMilk() {
-        actionsQueue.add(Actions.GET_MILK);
-    }
-    public enum Actions {
-        OPEN,
-        CLOSE,
-        GET_MILK
+        fridge.open();
+        fridge.getMilk();
+        fridge.close();
     }
 }
